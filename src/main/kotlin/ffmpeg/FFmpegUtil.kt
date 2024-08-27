e5e6fe17f28939743da32d4ec711ab23f889a782
shimin tang <tang_shimin@qq.com>
2024-08-28 00:06:58 +0800

    压缩 ffmpeg

--- src/main/kotlin/ffmpeg/FFmpegUtil.kt
+++ src/main/kotlin/ffmpeg/FFmpegUtil.kt
@@ -1,19 +1,38 @@
 package ffmpeg
 
-import net.bramp.ffmpeg.FFmpeg
 import java.io.File
 import java.util.*
 
-fun main() {
-    val fFmpeg = FFmpeg(findFFmpegPath())
-    println("ffmpeg exists:"+fFmpeg.isFFmpeg)
-}
 
 fun findFFmpegPath(): String {
-    val path: String = if(isWindows()){
-        getResourcesFile("ffmpeg/ffmpeg.exe").absolutePath
+    val path: String
+    if(isWindows()){
+        path = getResourcesFile("ffmpeg/ffmpeg.exe").absolutePath
     }else{
-        getResourcesFile("ffmpeg/ffmpeg").absolutePath
+
+        path =  getResourcesFile("ffmpeg/ffmpeg").absolutePath
+        if(!File(path).exists()){
+            println("ffmpeg not unzipped")
+            val zipFile = getResourcesFile("ffmpeg.zip")
+            // 必须使用系统命令行解压缩，否则会报错
+            val process = Runtime.getRuntime().exec("unzip -o ${zipFile.absolutePath} -d ${zipFile.parentFile.absolutePath}")
+            process.waitFor()
+            if (!process.isAlive){
+                println("unzip ffmpeg success")
+                val property = "compose.application.resources.dir"
+                val dir = System.getProperty(property)
+                //打包之后的环境，幕境已经安装到 macOS 了。
+                if (dir != null) {
+                    zipFile.delete()
+                    println("delete ffmpeg.zip file")
+                }
+
+            }
+
+
+        }
+
+
     }
     return path
 }
