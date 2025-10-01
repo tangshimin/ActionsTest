6f568a2771712a7ef92c76a63ca12185959f3c5d
tang shimin <tang_shimin@qq.com>
2025-10-02 00:57:20 +0800

    更新 TestApp.kt

--- src/test/kotlin/TestApp.kt
+++ src/test/kotlin/TestApp.kt
@@ -3,6 +3,8 @@ import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.performClick
 import org.junit.Rule
 import org.junit.Test
+import java.io.PrintStream
+import java.nio.charset.StandardCharsets
 
 class TestApp {
     @get:Rule
@@ -15,7 +17,15 @@ class TestApp {
         composeTestRule.setContent {
             App()
         }
-        println("测试中文字符显示")
+        
+        // 强制使用 UTF-8 编码输出中文
+        val originalOut = System.out
+        try {
+            System.setOut(PrintStream(System.out, true, StandardCharsets.UTF_8.name()))
+            println("测试中文字符显示")
+        } finally {
+            System.setOut(originalOut)
+        }
 
         composeTestRule.onNode(hasText("Hello, Github"))
             .assertExists()
